b31d5b953f0036d84054b20addf4a3a1c8dae24c
kim0504 <81956540+kim0504@users.noreply.github.com>
2023-03-11 15:24:32 +0900

    Name of methods are changed

--- GUI_2nd/kitech_gui/scheduler/directory.py
+++ GUI_2nd/kitech_gui/scheduler/directory.py
@@ -1,6 +1,6 @@
 import os
 
-class dir_info():
+class Directory():
     def __init__(self):
         self._file_path = '../'
         self._file_list = self.get_only_tdms()
@@ -8,27 +8,28 @@ class dir_info():
     def dir_list(self):
         print(self.get_only_tdms())
 
-    def inner_list(self):
+    def used_list(self):
         print(self._file_list)
         return self._file_list
 
-    def update_dir_list(self):
+    def update_list(self):
         self._file_list = self.get_only_tdms()
         print("dir update")
         print(self._file_list)
 
-    def get_new_file(self):
+    def search_new_file(self):
         print("current : ", self.get_only_tdms())
         print("past : ", self._file_list)
         return list(set(self.get_only_tdms()) - set(self._file_list))
+
+    def get_only_tdms(self):
+        return [file for file in os.listdir(self._file_path) if file.endswith(".tdms")]
 
     def create_result_txt(self, total:int, normal:int):
         result_path = '../result'
         if 'result' not in os.listdir('../'):
             os.mkdir("../result")
-        with open('/'.join([result_path, ".".join([str(self.get_new_file()),'txt'])]), 'w') as f:
+        with open('/'.join([result_path, ".".join([str(self.search_new_file()),'txt'])]), 'w') as f:
             f.write(f'{" ".join([str(total),str(normal),str(total-normal)])}')
             f.close()
-
-    def get_only_tdms(self):
-        return [file for file in os.listdir(self._file_path) if file.endswith(".tdms")]
+        return 0
