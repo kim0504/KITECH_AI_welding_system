7683cc782826801c22cafe8176591bdbddc1b564
kim0504 <81956540+kim0504@users.noreply.github.com>
2023-03-11 15:26:54 +0900

    Update time is changed from 10s to 60s

--- GUI_2nd/main.py
+++ GUI_2nd/main.py
@@ -1,14 +1,15 @@
 import sys
 from PyQt5.QtWidgets import QApplication
-from apscheduler.schedulers.qt import QtScheduler
+
 from kitech_gui.gui import gui
+from kitech_gui.scheduler import scheduler
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     gui_system = gui.GUI()
 
-    scheduler = QtScheduler()
-    scheduler.add_job(gui_system.update, 'interval', seconds=60)
+    scheduler = scheduler.WeldingScheduler(gui_system)
+    scheduler.add_job(scheduler.update, 'interval', seconds=60)
     scheduler.start()
 
     sys.exit(app.exec_())
